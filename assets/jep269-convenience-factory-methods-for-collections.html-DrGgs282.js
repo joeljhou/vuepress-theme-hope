bd4a89026f8cc775749f9a4d8a5bb768fa147345
joeljhou <45135095+joeljhou@users.noreply.github.com>
2023-12-20 08:24:04 +0000

    Deploying to gh-pages from @ joeljhou/joeljhou@8ed741d4b835aad548966011117809034e632859 🚀

---
+++ assets/jep269-convenience-factory-methods-for-collections.html-DrGgs282.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./plugin-vue_export-helper-x3n3nnut.js";import{o,c}from"./app-a5Fr3M3c.js";const t={};function n(r,s){return o(),c("div")}const a=e(t,[["render",n],["__file","jep269-convenience-factory-methods-for-collections.html.vue"]]);export{a as default};
